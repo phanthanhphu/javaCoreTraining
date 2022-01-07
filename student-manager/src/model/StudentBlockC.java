fc185d24203e5746e58ef7e7f6f6175d551884c4
phanthanhphu <86416071+phanthanhphu@users.noreply.github.com>
2022-01-07 09:01:06 +0700

    Update StudentBlockC.java

--- student-manager/src/model/StudentBlockC.java
+++ student-manager/src/model/StudentBlockC.java
@@ -1,9 +1,9 @@
 package model;
 
 public class StudentBlockC extends Person {
-	public static final String literature_Subject = "Literature";
-	public static final String History_Subject = "History";
-	public static final String Geography_Subject = "Geography";
+	public static final String LITERATURE_SUBJECT = "Literature";
+	public static final String HISTORY_SUBJECT = "History";
+	public static final String GEOGRAPHY_SUBJECT = "Geography";
 	public StudentBlockC(String id, String name, String address, int priority) {
         super(id, name, address, priority);
     }
@@ -14,7 +14,7 @@ public class StudentBlockC extends Person {
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 ", address='" + address + '\'' +
-                ", priority=" + priority + ", Subject: " + literature_Subject + " - " + History_Subject + " - " + Geography_Subject +
+                ", priority=" + priority + ", Subject: " + LITERATURE_SUBJECT + " - " + HISTORY_SUBJECT + " - " + GEOGRAPHY_SUBJECT +
                 '}';
     }
 

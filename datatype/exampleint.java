482ab055561883916a4d8aa1b53566e9d472fbaa
your name <ptp2656@gmail.com>
2022-01-16 12:32:52 +0700

    Feature/datatype

---
+++ datatype/exampleint.java
@@ -0,0 +1,10 @@
+package datatype;
+
+public class exampleint {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.print(c);
+	}
+}
---
+++ datatype/exampleshort.java
@@ -0,0 +1,8 @@
+package datatype;
+
+public class exampleshort {
+	public static void main(String[] args) {
+		short num = 'a';
+		System.out.print("num " + num);
+	}
+}

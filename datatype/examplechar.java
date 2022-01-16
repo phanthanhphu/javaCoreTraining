acf2724cf9f6f7e6b49cd259d5903f084c5b6d2c
your name <ptp2656@gmail.com>
2022-01-16 12:49:08 +0700

    Feature/datatype

---
+++ datatype/examplechar.java
@@ -0,0 +1,10 @@
+package datatype;
+
+public class examplechar {
+	public static void main(String[] args) {
+		char[] arrayList = {'T','r','y'};
+		String list = new String(arrayList);
+		System.out.println(list);
+		
+	}
+}
